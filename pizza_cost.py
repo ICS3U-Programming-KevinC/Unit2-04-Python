7f02e990b9f2443680a00ceaa8906ad082da13c8
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-09-28 23:37:14 +0000

    added coments

--- pizza_cost.py
+++ pizza_cost.py
@@ -4,13 +4,15 @@
 # Date: Sep. 28, 2022
 # This program calculates the cost of a pizza with user defined diameter
 
-
+# import constants for HST
 import constants
 
 
 def main():
+    # asks user for diameter
     diameter = float(input("What is the diameter of your pizza in inches? "))
     print("")
+    # prints total cost
     print(
         "the cost of your pizza is ${:.2f}".format(
             (1.5 * diameter + 4.25) * (constants.HST + 1)
